7da94ff89f2653db7fb93c41f2f5f0e6487de0cd
Sasha Kaminska <sasha249133@ukr.net>
2017-09-03 17:28:22 +0300

    Описала поцедуру ввода пользовательской информации

--- src/OutputUserInfo.java
+++ src/OutputUserInfo.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class OutputUserInfo {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Введите имя: ");
+        String name = in.nextLine();
+
+        System.out.println("Введите город проживания: ");
+        String city = in.nextLine();
+
+        System.out.println("Введите возраст: ");
+        int age = in.nextInt();
+        in.nextLine();
+
+        System.out.println("Напишите свое хобби: ");
+        String hobby = in.nextLine();
+
+    }
 }
