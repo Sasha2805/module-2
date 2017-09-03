992d8d0cc0757179023031a6a967d30d3c144710
Sasha Kaminska <sasha249133@ukr.net>
2017-09-03 18:06:56 +0300

    Вывела информацию о пользователе в консоль

--- src/OutputUserInfo.java
+++ src/OutputUserInfo.java
@@ -19,5 +19,22 @@ public class OutputUserInfo {
         System.out.println("Напишите свое хобби: ");
         String hobby = in.nextLine();
 
+        System.out.println("Имя: " + name);
+        System.out.println("Город: " + city);
+        System.out.println("Возраст: " + age);
+        System.out.println("Хобби: " + hobby);
+
+        System.out.println("----------------------------");
+
+        System.out.println("Человек по имени " + name + " живет в городе " + city + ".");
+        System.out.println("Этому человеку " + age + " лет и его хобби " + hobby + ".");
+
+        System.out.println("----------------------------");
+
+        System.out.println(name + " - " + "имя");
+        System.out.println(city + " - " + "город");
+        System.out.println(age + " - " + "возраст");
+        System.out.println(hobby + " - " + "хобби");
+        
     }
 }
